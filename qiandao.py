9a9deed93cda8d34fbfb2ab334d19e97618076cc
CnAiTewang <166534648+CnAiTewang@users.noreply.github.com>
2024-04-15 09:48:48 +0800

    Update qiandao.py

--- qiandao.py
+++ qiandao.py
@@ -1,54 +1,51 @@
-import requests, json, time, hmac, hashlib, base64, urllib.parse, os, requests, smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+import os
+import re
+from DecryptLogin import login
+from DecryptLogin.platforms.music163 import Cracker
 
-'''
-Csdn签到和抽奖
-'''
-
-
-class Csdn:
-    def __init__(self):
+'''网易云音乐自动签到'''
+class NeteaseSignin():
+    def __init__(self, username, password, **kwargs):
+        self.username = username
+        self.session = NeteaseSignin.login(username, password)
+        self.csrf = re.findall('__csrf=(.*?) for', str(self.session.cookies))[0]
+        self.cracker = Cracker()
         self.headers = {
-            'content-type': 'application/json;charset=UTF-8',
-            'cookie': "",
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4483.0 Safari/537.36'
-        }
-        self.data = {
-            "ip": "",
-            "platform": "pc-my",
-            "product": "pc",
-            "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4483.0 Safari/537.36",
-            "username": USERNAME,
-            "uuid": ""
-        }
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Referer': 'http://music.163.com/discover',
+                        'Accept': '*/*'
+                    }
+    '''外部调用'''
+    def run(self):
+        # 签到接口
+        signin_url = 'https://music.163.com/weapi/point/dailyTask?csrf_token=' + self.csrf
+        # 模拟签到(typeid为0代表APP上签到, 为1代表在网页上签到)
+        typeids = [0, 1]
+        for typeid in typeids:
+            client_name = 'Web端' if typeid == 1 else 'APP端'
+            # --构造请求获得响应
+            data = {
+                        'type': typeid
+                    }
+            data = self.cracker.get(data)
+            res = self.session.post(signin_url, headers=self.headers, data=data)
+            res_json = res.json()
+            # --判断签到是否成功
+            if res_json['code'] == 200:
+                print('[INFO]: 账号%s在%s签到成功...' % (self.username, client_name))
+            else:
+                print('[INFO]: 账号%s在%s签到失败, 原因: %s...' % (self.username, client_name, res_json.get('msg')))
+    '''模拟登录'''
+    @staticmethod
+    def login(username, password):
+        lg = login.Login()
+        _, session = lg.music163(username, password)
+        return session
 
-    def draws(self):
-        self.headers[
-            'cookie'] = COOKIE_DRAW
-        self.data['uuid'] = '10_19718702780-1615518137009-545439'
-        res = requests.post('https://me.csdn.net/api/LuckyDraw_v2/goodLuck', headers=self.headers, data=self.data)
-        data_draw = json.loads(res.text)
-        if data_draw['code'] == 200:
-            return data_draw['data']
-        else:
-            return '抽奖失败'
-
-    def signed(self):
-        self.headers[
-            'cookie'] = COOKIE_SIGNED
-        self.data['uuid'] = '10_10213081380-1623757365901-112902'
-        res = requests.post('https://me.csdn.net/api/LuckyDraw_v2/signIn', headers=self.headers, data=self.data)
-        return json.loads(res.text)
-
-
-if __name__ == "__main__":
-    COOKIE_DRAW = os.environ["COOKIE_DRAW"]  # 点击签到后在控制台从heard里面找到COOKIE
-    COOKIE_SIGNED = os.environ["COOKIE_SIGNED"]  # 点击签到后在控制台从heard里面找到COOKIE
-    USERNAME = os.environ["USERNAME"]  # 这里是’CSDN‘的用户名，链接后面的
-    QQ_SEND = os.environ['QQ_SEND']  # qq发件人信息格式  邮箱+smtp码 比如：iui9@qq.com+********(加号为分隔符)
-    QQ_ACCEPT = os.environ['QQ_ACCEPT']  # qq收件人邮箱地址
-    t = str(Csdn().draws())
-    r = str(Csdn().signed())
-    if len(QQ_ACCEPT) != 0:
-        Csdn().QQ_notice()
+'''run'''
+if __name__ == '__main__':
+    username = os.environ["NETEASE_USERNAME"]
+    password = os.environ["NETEASE_PASSWORD"]
+    sign_in = NeteaseSignin(username=username, password=password)
+    sign_in.run()
